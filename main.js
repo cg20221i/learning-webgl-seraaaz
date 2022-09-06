76dc68ddadce1c1662b877415b104c4dfb8d6b93
seraaaz <selomita.zhafiirah@gmail.com>
2022-09-06 15:14:01 +0700

    add color

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 function main(){
     var canvas = document.getElementById("myCanvas");
-    
+    var gl = canvas.getContext("webgl");
+
+    // VERTEX SHADER
+    var vertexShaderCode = 
+    "void main() {" + 
+    "}";
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
+    //FRAGMENT SHADER
+    var fragmentShaderCode = `
+    void main(){
+
+    }`;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+     // Comparing to C-Programming, we may imagine
+    //  that up to this step we have created two
+    //  object files (.o), for the vertex and fragment shaders
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+                //red, Green, Blue, Alpha (transparancy)
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
