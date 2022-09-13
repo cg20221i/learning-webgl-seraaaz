15dd1a796dc4835a9bfe81d2463fd9acf0dba766
seraaaz <selomita.zhafiirah@gmail.com>
2022-09-13 15:01:15 +0700

    draw a square

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function main(){
     var vertices = [
         0.5, 0.5, 
         0.0, 0.0, 
-        -0.5, -0.5
+        -0.5, 0.5,
+        0.0, 1.0
     ];
 
     // create linked-list for storing the vertices data in the GPU realm
@@ -67,7 +68,7 @@ function main(){
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINT, 0, 3);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 
     //(mode, first, count);
 }
